b8f7297306993fe46de8835e8acff9e7628de31c
PavloYurchyshynn <gamepaha123@gmail.com>
2023-03-03 11:04:25 +0200

    feature/MP-30/feat: add repositories service to program file

---
+++ src/Marketplace.API/DependencyInjection/DIConfig.cs
@@ -0,0 +1,21 @@
+﻿using Marketplace.DataAccess.Repositories.Contracts;
+
+namespace Marketplace.API.DependencyInjection
+{
+    public static class DependencyInjectionConfiguration
+    {
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<ICartItemRepository, CartItemRepository>();
+            services.AddScoped<ICartRepository, CartRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICommentRepository, CommentRepository>();
+            services.AddScoped<ICustomerInfoRepository, CustomerInfoRepository>();
+            services.AddScoped<IOrderRepository, OrderRepository>();
+            services.AddScoped<IPaymentMethodRepository, PaymentMethodRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IPromocodeRepository, PromocodeRepository>();
+        }
+    }
+}
